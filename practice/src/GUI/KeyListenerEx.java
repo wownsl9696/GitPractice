799a28cc40ef8e078af69e475e453d179f7a0079
Juns <62887886+wownsl9696@users.noreply.github.com>
2021-02-06 17:56:27 +0900

    Revert "Revert "Layout""
    This reverts commit 6b53e53e60d0d7ae99e7984b1f8e41e231567822.

---
+++ practice/src/GUI/KeyListenerEx.java
@@ -0,0 +1,62 @@
+package GUI;
+
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class KeyListenerEx extends JFrame {
+	private JLabel [] keyMessage;
+	
+	public KeyListenerEx() {
+		setTitle("keyListener 예제");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		Container c = getContentPane();
+		c.setLayout(new FlowLayout());
+		c.addKeyListener(new MyKeyListener());
+		
+		keyMessage = new JLabel[3];
+		keyMessage[0] = new JLabel(" getKeyCode() ");
+		keyMessage[1] = new JLabel(" getKeyCode() ");
+		keyMessage[2] = new JLabel(" getKeyCode() ");
+		
+		for(int i = 0; i<keyMessage.length; i++) {
+			c.add(keyMessage[i]);
+			keyMessage[i].setOpaque(true);
+			keyMessage[i].setBackground(Color.YELLOW);
+		}
+		
+		setSize(300, 150);
+		setVisible(true);
+		
+		c.setFocusable(true);
+		c.requestFocus();
+	}
+	
+	class MyKeyListener extends KeyAdapter{
+		public void keyPressed(KeyEvent e) {
+			int keyCode = e.getKeyCode();
+			char keyChar = e.getKeyChar();
+			keyMessage[0].setText(Integer.toString(keyCode));
+			keyMessage[1].setText(Character.toString(keyChar));
+			keyMessage[2].setText(KeyEvent.getKeyText(keyCode));
+			
+			System.out.println("KeyPressed");
+		}
+		public void keyReleased(KeyEvent e) {
+			System.out.println("KeyReleased");
+		}
+		public void keyTyped(KeyEvent e) {
+			System.out.println("KeyTyped");
+		}
+	}
+	public static void main(String[] args) {
+		new KeyListenerEx();
+	}
+
+}
